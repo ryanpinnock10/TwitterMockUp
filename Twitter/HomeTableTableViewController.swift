c0f64e8067795fbce823464e0f840e8528fe6232
ryanPinnock <ryanpinnock10@gmail.com>
2019-11-01 03:15:45 -0400

    Login and logout functionality configured

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Ryan Pinnock on 11/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    @IBAction func logoutButton(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
