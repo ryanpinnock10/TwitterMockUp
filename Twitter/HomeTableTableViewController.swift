b6296b2095e92ae6808c7d8b4e4f6c7ff4a774d8
ryanPinnock <ryanpinnock10@gmail.com>
2019-11-02 11:03:46 -0400

    Refresh works!

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -13,10 +13,21 @@ class HomeTableTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
     
-    func loadTweets()
+    let myRefreshControl = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweets()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    
+    @objc func loadTweets()
     {
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": 20]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: {(tweets: [NSDictionary]) in
             
@@ -26,6 +37,7 @@ class HomeTableTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
             print("Failure to load")
@@ -58,10 +70,7 @@ class HomeTableTableViewController: UITableViewController {
         return cell
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadTweets()
-    }
+    
 
     // MARK: - Table view data source
 
